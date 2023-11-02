0935044c672b347dd34bfada339f3935bd2f0087
Rhys Powell <55314665+powellrhys@users.noreply.github.com>
2023-11-02 14:56:24 +0000

    Feature/initial commit (#1)
    * initial commit
    
    * yamllint added
    
    * truhty error ignored from yamllint
    
    * requirements.txt added
    
    * unit tests added to github actions
    
    * bug fix in pipeline
    
    * folder structure refactor
    
    * code turned into modules
    
    * code coverage added
    
    * code coverage added
    
    * yamllinting
    
    * code coverage added to requirements
    
    * pipeline debugging
    
    * code-coverage added
    
    * code coverage removed

---
+++ tests/unit_tests.py
@@ -0,0 +1,18 @@
+from functions.functions import add
+
+
+def test_add():
+    '''
+    Input: No Input
+    Output: Assertion of test result
+    Function to test the functionality of the add function
+    '''
+    # Define input variable
+    x = 3
+    y = 4
+
+    # Define expected result
+    expected_result = 7
+
+    # Check function result with expected result
+    assert add(x, y) == expected_result
