21a72c1047cfeaa37f3ea5d1c18b2a3c8547080d
rhys powell <rhys.powell2@btinternet.com>
2023-11-02 14:00:24 +0000

    initial commit

---
+++ functions.py
@@ -0,0 +1,8 @@
+def add(x: int = 1, y: int = 2):
+    '''
+    Inputs: integers to add together
+    Outputs: Sum of both inputs
+    Function to add both inputs together
+    '''
+
+    return x + y
